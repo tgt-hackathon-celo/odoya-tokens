19e0c087255a9d6ed9db1d4033a30ca1f516ba7f
Jeff Prestes <jeffprestes@gmail.com>
2022-12-07 09:41:46 -0300

    deploy na celo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,25 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0"
+      },
+      chainId: 44787
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      },
+      chainId: 5
+    }
+  }
 };
